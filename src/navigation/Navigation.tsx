fa2780f0e515445b216750a60103a93d62a7f845
Александр Утбанов <utbanovag@it2g.ru>
2021-08-15 20:25:15 +0300

    add hendler for back button

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useModals } from "react-mobile-modals";
+import {App} from "@capacitor/app";
+
 import {TabNavigation} from "./TabNavigation";
 
 export const Navigation = () => {
+    const {closeModal} = useModals();
+
+    useEffect(() => {
+        App.addListener('backButton', closeModal);
+    }, [])
+
     return <TabNavigation />
 }
